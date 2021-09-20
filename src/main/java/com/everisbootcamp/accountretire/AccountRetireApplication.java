387733ccabea837c52090dbb35cb82503839860a
umb.kevsidorov@gmail.com <umb.kevsidorov@gmail.com>
2021-09-20 12:34:12 -0500

    Created project

---
+++ src/main/java/com/everisbootcamp/accountretire/AccountRetireApplication.java
@@ -0,0 +1,13 @@
+package com.everisbootcamp.accountretire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountRetireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountRetireApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/everisbootcamp/accountretire/AccountRetireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everisbootcamp.accountretire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountRetireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
